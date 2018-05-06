3b65b95eca19322d97ef0495e1083ca16a87cc12
muhammadahmed1122 <ahmed.khan853@yahoo.com>
2018-05-06 03:48:31 -0700

    search edit delete detaile create

---
+++ WebApplication4/Controllers/Default1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        private ahmedEntities db = new ahmedEntities();
+
+        // GET: /Default1/
+        public ActionResult Index()
+        {
+            return View(db.products.ToList());
+        }
+
+        // GET: /Default1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: /Default1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Default1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="id,name,address,decs")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: /Default1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: /Default1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="id,name,address,decs")] product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: /Default1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: /Default1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication4/Controllers/homeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class homeController : Controller
+    {
+        ahmedEntities db = new ahmedEntities();
+        //
+        // GET: /home/
+        public ActionResult Home()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Home(product pro)
+        {
+            db.products.Add(pro);
+            db.SaveChanges();
+            return RedirectToAction("SHOW");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+             if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(product pro)
+        {
+            db.Entry(pro).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("SHOW");
+        }
+
+
+    [HttpGet]
+        public ActionResult det(int id)
+        {
+            product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("SHOW");
+        }
+
+        [HttpGet]
+    public ActionResult detl(int? id)
+    {
+        if (id == null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        product product = db.products.Find(id);
+        if (product == null){
+        
+            return HttpNotFound();
+        }
+            return View(product);
+    }
+
+        [HttpGet]
+        public ActionResult SHOW(string searching)
+        {
+            return View(db.products.Where(a => a.name.Contains(searching) || searching == null).ToList());
+        } 
+
+	}
+
+}
